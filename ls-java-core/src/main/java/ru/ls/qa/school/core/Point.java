6db001eb85cf2a7a7c216a5ccdbded45dcaabf0a
Ivan Tavintsev <tavinzeva@gmail.com>
2025-05-11 19:30:38 +0300

    Задание 2: Потренироваться использовать функции, классы, объекты и методы

---
+++ ls-java-core/src/main/java/ru/ls/qa/school/core/Point.java
@@ -0,0 +1,17 @@
+package ru.ls.qa.school.core;
+
+public class Point {
+  public double x;
+  public double y;
+public Point (double x, double y ) {
+  this.x = x;
+  this.y = y;
+}
+public double distance(Point point){
+  double xcor = this.x -point.x; //  координаты х
+  double ycor = this.y - point.y; // координаты y
+
+  return Math.sqrt((xcor * xcor) + (ycor * ycor));
+
+}
+}
