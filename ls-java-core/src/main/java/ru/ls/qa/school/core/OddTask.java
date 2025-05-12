2f8acd046f9b89e559329cb95ab5b6882d0bd216
13th <pavel.voronin@ligastavok.ru>
2025-05-12 14:18:51 +0300

    Паша: правки форматирования

--- ls-java-core/src/main/java/ru/ls/qa/school/core/OddTask.java
+++ ls-java-core/src/main/java/ru/ls/qa/school/core/OddTask.java
@@ -1,14 +1,17 @@
 package ru.ls.qa.school.core;
 
 public class OddTask {
-  public static boolean isOdd(int number) {
-    return number % 2 != 0;
-  }
 
-  public static void main(String[] args) {
-    int[] tNumber = {1, 3, 5, 4};
-    for (int number : tNumber) {
-      System.out.println("Число " + number + " нечетное? " + isOdd(number));
+    public static boolean isOdd(int number) {
+        return number % 2 != 0;
     }
-  }
+
+    public static void main(String[] args) {
+
+        int[] tNumber = { 1, 3, 5, 4 };
+
+        for (int number : tNumber) {
+            System.out.println("Число " + number + " нечетное? " + isOdd(number));
+        }
+    }
 }
--- ls-java-core/src/main/java/ru/ls/qa/school/core/Point.java
+++ ls-java-core/src/main/java/ru/ls/qa/school/core/Point.java
@@ -1,17 +1,19 @@
 package ru.ls.qa.school.core;
 
 public class Point {
-  public double x;
-  public double y;
-public Point (double x, double y ) {
-  this.x = x;
-  this.y = y;
-}
-public double distance(Point point){
-  double xcor = this.x -point.x; //  координаты х
-  double ycor = this.y - point.y; // координаты y
+    public double x;
+    public double y;
 
-  return Math.sqrt((xcor * xcor) + (ycor * ycor));
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 
-}
+    public double distance(Point point) {
+        double xcor = this.x - point.x; //  координаты х
+        double ycor = this.y - point.y; // координаты y
+
+        return Math.sqrt((xcor * xcor) + (ycor * ycor));
+
+    }
 }
